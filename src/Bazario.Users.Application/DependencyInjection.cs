cdb3ba7a28601792fe2807097772559e14cd2bda
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-04-30 16:38:52 +0300

    Initialize CA structure

---
+++ src/Bazario.Users.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bazario.Users.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/Bazario.Users.WebAPI/Program.cs
@@ -0,0 +1,34 @@
+using Bazario.Users.Application;
+using Bazario.Users.Infrastructure;
+
+namespace Bazario.Users.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddApplication();
+            builder.Services.AddInfrastructure();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
