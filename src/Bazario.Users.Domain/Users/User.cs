a3fad572fdeb76530b7c67c8865ff6b3318d1057
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-05-01 11:36:15 +0300

    Add User Aggregate Root

--- src/Bazario.Users.Domain/Users/User.cs
+++ src/Bazario.Users.Domain/Users/User.cs
@@ -1,4 +1,11 @@
-﻿using Bazario.AspNetCore.Shared.Domain;
+﻿using Bazario.AspNetCore.Shared.Auth.Roles;
+using Bazario.AspNetCore.Shared.Domain;
+using Bazario.AspNetCore.Shared.Results;
+using Bazario.Users.Domain.Users.BirthDates;
+using Bazario.Users.Domain.Users.Emails;
+using Bazario.Users.Domain.Users.FirstNames;
+using Bazario.Users.Domain.Users.LastNames;
+using Bazario.Users.Domain.Users.PhoneNumbers;
 
 namespace Bazario.Users.Domain.Users
 {
@@ -7,5 +14,53 @@ namespace Bazario.Users.Domain.Users
         private User()
             : base(new(Guid.Empty))
         { }
+
+        private User(
+            UserId userId,
+            Role role,
+            FirstName firstName,
+            LastName lastName,
+            Email email,
+            PhoneNumber phoneNumber,
+            BirthDate birthDate) : base(userId)
+        {
+            Role = role;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
+        }
+
+        public Role Role { get; }
+
+        public FirstName FirstName { get; private set; }
+
+        public LastName LastName { get; private set; }  
+
+        public Email Email { get; private set; }
+
+        public PhoneNumber PhoneNumber { get; private set; }
+
+        public BirthDate BirthDate { get; private set; }
+
+        public static Result<User> Create(
+            UserId userId,
+            Role role,
+            FirstName firstName,
+            LastName lastName,
+            Email email,
+            PhoneNumber phoneNumber,
+            BirthDate birthDate)
+        {
+            return new User(
+                userId, 
+                role,
+                firstName, 
+                lastName, 
+                email, 
+                phoneNumber, 
+                birthDate);
+        }
     }
 }
