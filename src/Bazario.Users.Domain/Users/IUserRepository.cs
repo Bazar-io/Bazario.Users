a48aa22a86af0bb65b84be1a0d8864e4a5a8f6ad
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-05-01 11:58:40 +0300

    Add Configuration and DbContext

---
+++ src/Bazario.Users.Domain/Users/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Bazario.Users.Domain.Users
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(
+            UserId id,
+            CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<User>> GetAllAdminsAsync(
+            CancellationToken cancellationToken = default);
+
+        Task<int> InsertAsync(
+            User user,
+            CancellationToken cancellationToken = default);
+
+        Task<int> UpdateAsync(
+            User user,
+            CancellationToken cancellationToken = default);
+
+        Task<int> DeleteAsync(
+            User user,
+            CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Bazario.Users.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Bazario.Users.Domain.Users;
+using Microsoft.Extensions.Logging;
+
+namespace Bazario.Users.Infrastructure.Persistence.Repositories
+{
+    internal sealed class UserRepository : IUserRepository
+    {
+        private readonly ILogger<UserRepository> _logger;
+
+        public UserRepository(ILogger<UserRepository> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<int> DeleteAsync(User user, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAdminsAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(UserId id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertAsync(User user, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(User user, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
