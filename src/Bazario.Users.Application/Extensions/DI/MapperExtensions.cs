8c8638411597b48af6690cf3cb736745c69ce097
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-05-01 14:57:44 +0300

    [BZRIO-89] Implement Get All Admins Endpoint (#3)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-89?atlOrigin=eyJpIjoiNDQ4OWVjYTdkOTNkNGUyOTliN2Q2ZDRjYzgyYTQzNTAiLCJwIjoiaiJ9
    
    * Add Query Handler
    * Implement Mapper
    * Add Controller
    
    ---------
    
    Co-authored-by: savchenkoaddev <p.savchenkoad.dev@gmail.com>

---
+++ src/Bazario.Users.Application/Extensions/DI/MapperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Bazario.AspNetCore.Shared.Infrastructure.Mappers.DependencyInjection;
+using Bazario.Users.Application.UseCases.Users.DTO;
+using Bazario.Users.Application.UseCases.Users.Mappers;
+using Bazario.Users.Domain.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bazario.Users.Application.Extensions.DI
+{
+    internal static class MapperExtensions
+    {
+        public static IServiceCollection AddMappers(this IServiceCollection services)
+        {
+            services.AddMapper<User, UserResponse, UserToUserResponseMapper>();
+
+            return services;
+        }
+    }
+}
