9a4245a84ee123166b34c9b9860047c9a5025d0c
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-05-02 10:23:26 +0300

    Configure and Add Migration

---
+++ src/Bazario.Users.Infrastructure/Migrations/20250502072136_BanDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bazario.Users.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class BanDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BanDetails_BlockedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BanDetails_ExpiresAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BanDetails_Reason",
+                table: "Users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BanDetails_BlockedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BanDetails_ExpiresAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BanDetails_Reason",
+                table: "Users");
+        }
+    }
+}
