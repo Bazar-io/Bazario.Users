a6e736456f9efbf8990faa6ac8f98e0c3c476fbe
clowd1e <sashapro712@gmail.com>
2025-05-14 22:28:06 +0200

    Configure MessageBroker

--- src/Bazario.Users.Infrastructure/DependencyInjection.cs
+++ src/Bazario.Users.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Bazario.Users.Infrastructure.Extensions.DI;
+﻿using Bazario.AspNetCore.Shared.Infrastructure.MessageBroker.DependencyInjection;
+using Bazario.Users.Infrastructure.Extensions.DI;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bazario.Users.Infrastructure
@@ -8,7 +9,11 @@ namespace Bazario.Users.Infrastructure
         public static IServiceCollection AddInfrastructure(
             this IServiceCollection services)
         {
+            var assembly = typeof(DependencyInjection).Assembly;
+
             services.AddAppOptions();
+
+            services.AddMessageBroker(assembly);
 
             services.AddPersistence();
 
