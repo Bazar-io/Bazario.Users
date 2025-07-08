f374aa77787dc0eff7abdc74620a26cb1e060d42
clowd1e <sashapro712@gmail.com>
2025-07-08 21:37:05 +0200

    Add application layer architecture tests

---
+++ tests/Bazario.Users.ArchitectureTests/Application/DomainEventHandlersTests.cs
@@ -0,0 +1,15 @@
+﻿using Bazario.AspNetCore.Shared.ArchitectureTests.Application;
+using Bazario.Users.ArchitectureTests.Storage;
+
+namespace Bazario.Users.ArchitectureTests.Application
+{
+    public sealed class DomainEventHandlersTests
+        : DomainEventHandlersTestsBase
+    {
+        public DomainEventHandlersTests()
+        {
+            SetTestAssembly(
+                TestAssembliesStorage.TestAssemblies.ApplicationAssembly);
+        }
+    }
+}
