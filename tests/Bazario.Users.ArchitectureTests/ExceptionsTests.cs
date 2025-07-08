322c6c3354f687dd612ce03555c14433cc47791d
clowd1e <sashapro712@gmail.com>
2025-07-08 21:36:48 +0200

    Add common architecture tests

---
+++ tests/Bazario.Users.ArchitectureTests/ExceptionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Bazario.AspNetCore.Shared.ArchitectureTests;
+using Bazario.Users.ArchitectureTests.Storage;
+
+namespace Bazario.Users.ArchitectureTests
+{
+    public sealed class ExceptionsTests : ExceptionsTestsBase
+    {
+        public ExceptionsTests()
+        {
+            SetTestAssemblies(
+                TestAssembliesStorage.TestAssemblies.ApplicationAssembly,
+                TestAssembliesStorage.TestAssemblies.InfrastructureAssembly,
+                TestAssembliesStorage.TestAssemblies.PresentationAssembly
+            );
+        }
+    }
+}
---
+++ tests/Bazario.Users.ArchitectureTests/LayersTests.cs
@@ -0,0 +1,13 @@
+﻿using Bazario.AspNetCore.Shared.ArchitectureTests.CleanArchitecture;
+using Bazario.Users.ArchitectureTests.Storage;
+
+namespace Bazario.Users.ArchitectureTests
+{
+    public sealed class LayersTests : LayersTestsBase
+    {
+        public LayersTests()
+        {
+            SetTestAssemblies(TestAssembliesStorage.TestAssemblies);
+        }
+    }
+}
